7bdbc66b946857423bfb8ea7238e362f7d9b660f
Daniel88V <daniel12velo@gmail.com>
2025-07-09 19:07:16 -0600

    Parte Daniel

--- Farmacia.py
+++ Farmacia.py
@@ -1,9 +1,9 @@
 print("Bienvenido a la farmacia")
 class Medicamento:
-    def __init__(self, nombre, tipo, FechaVec):
+    def __init__(self, nombre, tipo, fechavec):
         self._nombre = nombre
         self._tipo = tipo
-        self._FechaVec = FechaVec
+        self._FechaVec = fechavec
         self._medicamento = []
     def ingreso_medicamento(self, medicamento):
         self._medicamento.append(medicamento)
