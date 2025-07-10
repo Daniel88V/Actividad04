5fe280ba4b08303cd6fb151aea480862d31fbc60
escartigui <escarlethmayali@gmail.com>
2025-07-09 19:16:59 -0600

    Parte Escar

--- Clinica.py
+++ Clinica.py
@@ -5,21 +5,6 @@ def menu():
         print("2.Atender Pacientes")
         print("3.Mostrar Cola")
         print("4.Salir")
-        print("Elija una opción")
-op = int(input())
-if op == 1:
-    print("Nombre del paciente")
-    nombre = input()
-    print("ID")
-    id = int(input())
-    print("No. Telefono")
-    telefono = input()
-elif op == 2:
-    print(f"el nombre del paciente {nombre}")
-    print(f"el telefono del paciente {telefono}")
-    print(f"Paciente {id} esta siendo atendido")
-elif op == 3:
-    print("Listado de los pacientes")
-if op == 4:
-    print("Estamos Saliendo")
-    
+        op = input("Elija una opción: ")
+
+
