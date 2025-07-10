177be0ebdb7b6a2ea3f8cbfb73af99c7a211fda7
Daniel88V <daniel12velo@gmail.com>
2025-07-09 19:24:57 -0600

    Parte Daniel

--- Farmacia.py
+++ Farmacia.py
@@ -1,4 +1,3 @@
-print("Bienvenido a la farmacia")
 class Medicamento:
     def __init__(self, nombre, tipo, fechavec):
         self._nombre = nombre
@@ -9,13 +8,29 @@ class Medicamento:
         self._medicamento.append(medicamento)
         print(f"Medicamento: {self._medicamento} agregado exitosamente")
     def __str__(self):
-        return f"Medicamento: {self._medicamento} Fecha de Vencimiento: {self._FechaVec}"
+        return f"Medicamento: {self._medicamento}| Tipo: {self._tipo}| Fecha de Vencimiento: {self._FechaVec}"
 class Farmacia:
     def __init__(self, nombre_farmacia):
         self._nombre_farmacia = nombre_farmacia
-        self.clientes = []
+        self.medicamento = []
+    def buscar_medicamento(self, nombre):
+        for medicina in self.medicamento:
+            if medicina._nombre == nombre:
+                return medicina
+        return None
+    def registrar_medicina(self):
+        print("---Registro de nuevo medicamento---")
+        nombre = input("Nombre del medicamento: ")
+        tipo = input("Tipo del medicamento: ")
+        fechavec = input("Fecha de vencimento: ")
+        if self.buscar_medicamento(fechavec):
+            print("Este medicamento ya existe")
+            return
+        nueva_medicina = Medicamento(nombre, tipo, fechavec)
+        self.medicamento.append(nueva_medicina)
+        print(f"Medicamento: {nombre} agregado exitosamente")
 def main():
-    mi_clinica = Medicamento("Farmacia de la clinica")
+    mi_clinica = Farmacia("Farmacia de la clinica")
     print(f"Bienvenido a la farmacia")
     while True:
         print(" ==== MENÚ FARMACIA ====")
@@ -25,7 +40,7 @@ def main():
         print("4. Salir")
         eleccion = input("Seleccione una opción: ")
         if eleccion == "1":
-            mi_clinica.ingreso_medicamento()
+            mi_clinica.buscar_medicamento()
         elif eleccion == "4":
             print("Saliendo del sistema...")
             break
