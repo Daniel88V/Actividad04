63ad196855e8d5ca8b28e4d9d99b362da225332b
escartigui <escarlethmayali@gmail.com>
2025-07-09 19:13:15 -0600

    Parte Escar

--- Clinica.py
+++ Clinica.py
@@ -1,12 +1,11 @@
 def menu:
     while True:
-
-print ("Bienvenido al menu de la Recepción")
-print("1.Agregar Pacientes")
-print("2.Atender Pacientes")
-print("3.Mostrar Cola")
-print("4.Salir")
-print("Elija una opción")
+        print ("Bienvenido al menu de la Recepción")
+        print("1.Agregar Pacientes")
+        print("2.Atender Pacientes")
+        print("3.Mostrar Cola")
+        print("4.Salir")
+        print("Elija una opción")
 op = int(input())
 if op == 1:
     print("Nombre del paciente")
