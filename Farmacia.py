096a6303c0b7b3c74b24ea60f3e1524923e0c590
Daniel88V <daniel12velo@gmail.com>
2025-07-09 19:03:40 -0600

    Parte Daniel

--- Farmacia.py
+++ Farmacia.py
@@ -8,4 +8,18 @@ class Medicamento:
     def ingreso_medicamento(self, medicamento):
         self._medicamento.append(medicamento)
         print(f"Medicamento: {self._medicamento} agregado exitosamente")
+    def __str__(self):
+        return f"Medicamento: {self._medicamento} Fecha de Vencimiento: {self._FechaVec}"
 
+def main():
+    mi_clinica = Medicamento("Farmacia de la clinica")
+    print(f"Bienvenido a la farmacia")
+    while True:
+        print(" ==== MENÚ FARMACIA ====")
+        print("1. Agregar medicamento")
+        print("2. Asignar medicamento")
+        print("3. Mostrar pila")
+        print("4. Salir")
+        eleccion = input("Seleccione una opción: ")
+        if eleccion == "1":
+            mi_clinica.ingreso_medicamento()
